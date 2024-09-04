9bd8eeca0523450335c713989fa4cf3e1c9632d6
Sofia Ex <sophia.exarchou@icloud.com>
2024-09-04 23:35:45 +0200

    naming more specific

--- game.js
+++ game.js
@@ -1,9 +1,22 @@
 const { getRandNumber } = require("./helper");
 
 // game logic
+const handShape = {
+  rock: 0,
+  paper: 1,
+  scissors: 2,
+  none: 3,
+};
+
 const getRandHandShape = () => getRandNumber(3);
 
-const isDraw = (choice1, choice2) => choice1 === choice2;
+const isDraw = (handOne, handTwo) => handOne === handTwo;
+
+const isHandOneWinner = (handOne, handTwo) =>
+  (handOne === handShape.rock && handTwo === handShape.scissors) ||
+  (handOne === handShape.scissors && handTwo === handShape.paper) ||
+  (handOne === handShape.paper && handTwo === handShape.rock);
+
 const getHandShapeNameFromNumber = (number) => {
   switch (number) {
     case 0:
@@ -17,18 +30,6 @@ const getHandShapeNameFromNumber = (number) => {
   }
 };
 
-const isComputerAWinner = (choice1, choice2) =>
-  (choice1 === handShape.rock && choice2 === handShape.scissors) ||
-  (choice1 === handShape.scissors && choice2 === handShape.paper) ||
-  (choice1 === handShape.paper && choice2 === handShape.rock);
-
-const handShape = {
-  rock: 0,
-  paper: 1,
-  scissors: 2,
-  none: 3,
-};
-
 if (process.argv.length !== 3) {
   console.error("Error! You typed more than one choice or no choice!");
   console.info(
@@ -39,27 +40,29 @@ if (process.argv.length !== 3) {
 }
 
 // https://nodejs.org/api/process.html#processargv
-// process.argv is always a string so userChoice input will also always be a string.
-// but computerChoice is always number .... bug:
+// process.argv is always a string so userHand input will also always be a string.
+// but AI hand is always number .... bug:
 // then isDraw does not work because  "2" === 2 is always false!!!
 // solution: converted the process.argv into anumber using parseInt
-const userChoice = parseInt(process.argv[2]);
+const userHand = parseInt(process.argv[2]);
 
-if (userChoice > 2) {
+if (userHand > 2) {
   console.error("Error! You typed a number higher than 2!");
   console.info(
-    "Please type a 0 or 1 or 2! (0 = rock, 1 = paper, 2 = scissors). Numbers higher than 2 are not valid."
+    "Please type a 0 or 1 or 2! (0 = rock, 1 = paper, 2 = scissors)"
   );
+  console.info("Numbers higher than 2 are not valid.");
   return;
 }
 
-const computerChoice = getRandHandShape();
+const aiHand = getRandHandShape();
 
-console.info(`Computer played ${getHandShapeNameFromNumber(computerChoice)}.`);
-console.info(`You played ${getHandShapeNameFromNumber(userChoice)}.`);
+console.info(`AI played ${getHandShapeNameFromNumber(aiHand)}.`);
+console.info(`You played ${getHandShapeNameFromNumber(userHand)}.`);
 
-if (isDraw(computerChoice, userChoice))
-  console.info("\x1b[35m", "It's a draw! You both win!");
-else if (isComputerAWinner(computerChoice, userChoice))
-  console.info("\x1b[31m", "Computer Wins!");
+if (isDraw(aiHand, userHand)) {
+  console.info("\x1b[35m", "It's a draw!");
+  console.info("\x1b[35m", "You both win!");
+} else if (isHandOneWinner(aiHand, userHand))
+  console.info("\x1b[31m", "AI Wins!");
 else console.info("\x1b[32m", "You Win!");
