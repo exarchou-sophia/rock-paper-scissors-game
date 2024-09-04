9e37de48200afd234d413bc00b8abb18443a13ca
Sofia Ex <sophia.exarchou@icloud.com>
2024-09-04 20:15:32 +0200

    game logic code and helper file

--- game.js
+++ game.js
@@ -1,1 +1,25 @@
+const { getRandNumber } = require("./helper");
+
 // game logic
+const handShape = {
+  rock: 0,
+  paper: 1,
+  scissors: 2,
+};
+
+const getRandHandShape = () => getRandNumber(3);
+
+const isDraw = (choice1, choice2) => choice1 === choice2;
+
+const isChoiceAWinner = (choice1, choice2) =>
+  (choice1 === handShape.rock && choice2 === handShape.scissors) ||
+  (choice1 === handShape.scissors && choice2 === handShape.paper) ||
+  (choice1 === handShape.paper && choice2 === handShape.rock);
+
+// some execution tests
+
+if (isDraw(getRandHandShape(), getRandHandShape()))
+  console.log("It's a draw! You both win!");
+else if (isChoiceAWinner(getRandHandShape(), getRandHandShape()))
+  console.log("ChoiceA Wins!");
+else console.log("ChoiceB Wins!");
