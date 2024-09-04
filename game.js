593bb496409325bf880e8d5aeacde3e185774d74
Sofia Ex <sophia.exarchou@icloud.com>
2024-09-04 20:44:35 +0200

    testing human player

--- game.js
+++ game.js
@@ -1,12 +1,6 @@
 const { getRandNumber } = require("./helper");
 
 // game logic
-const handShape = {
-  rock: 0,
-  paper: 1,
-  scissors: 2,
-};
-
 const getRandHandShape = () => getRandNumber(3);
 
 const isDraw = (choice1, choice2) => choice1 === choice2;
@@ -16,10 +10,22 @@ const isChoiceAWinner = (choice1, choice2) =>
   (choice1 === handShape.scissors && choice2 === handShape.paper) ||
   (choice1 === handShape.paper && choice2 === handShape.rock);
 
+const handShape = {
+  rock: 0,
+  paper: 1,
+  scissors: 2,
+};
+
+// TODO use params
+const userChoice = handShape.rock;
+const computerChoice = getRandHandShape();
+
+console.log(computerChoice, userChoice);
+
 // some execution tests
 
-if (isDraw(getRandHandShape(), getRandHandShape()))
+if (isDraw(computerChoice, userChoice))
   console.log("It's a draw! You both win!");
-else if (isChoiceAWinner(getRandHandShape(), getRandHandShape()))
+else if (isChoiceAWinner(computerChoice, userChoice))
   console.log("ChoiceA Wins!");
 else console.log("ChoiceB Wins!");
